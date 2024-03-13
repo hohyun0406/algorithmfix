578d79abdf613e9ea33c5086c12445c3264152e3
hohyun0406 <hohyun0406@gmail.com>
2024-03-13 18:26:08 +0900

    beokT

---
+++ src/baekjoonPractice/no1000.java
@@ -0,0 +1,14 @@
+package baekjoonPractice;
+
+import java.util.Scanner;
+
+public class no1000 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+    }
+}
