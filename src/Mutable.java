95ef96fc90dd96281de66f25516bdeda51223fa5
hohyun0406 <hohyun0406@gmail.com>
2024-03-18 19:46:39 +0900

    240318 daily

---
+++ src/Mutable.java
@@ -0,0 +1,10 @@
+public class Mutable {
+    public static void main(String[] args) {
+        String a = "Hello World";
+        System.out.println(a.hashCode());
+        String b = "Hello World";
+        a+="555";
+
+    }
+
+}
---
+++ src/baekjoonPractice/No10926.java
@@ -0,0 +1,11 @@
+package baekjoonPractice;
+
+import java.util.Scanner;
+
+public class No10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next()+"??!");
+    }
+
+}
