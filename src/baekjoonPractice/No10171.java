8c5999a028d60a58f082271cfd9c63fb93bee70d
hohyun0406 <hohyun0406@gmail.com>
2024-03-27 19:29:39 +0900

    240328 neko

---
+++ src/baekjoonPractice/No10171.java
@@ -0,0 +1,11 @@
+package baekjoonPractice;
+
+public class No10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" +
+                            " )  ( ')\n" +
+                           "(  /  )\n" +
+                           " \\(__)|");
+
+    }
+}
