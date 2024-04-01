05085088b441c13811271ad104cf51cc286efe66
hohyun0406 <hohyun0406@gmail.com>
2024-04-01 21:45:09 +0900

    dog

---
+++ src/baekjoonPractice/No10172.java
@@ -0,0 +1,11 @@
+package baekjoonPractice;
+
+public class No10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+    }
+}
