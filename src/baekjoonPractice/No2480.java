19a936f19d9ed0f7e125417aa3a3d81f5da785dc
hohyun0406 <hohyun0406@gmail.com>
2024-04-04 20:20:22 +0900

    240404 No2480 3dice

--- src/baekjoonPractice/No2480.java
+++ src/baekjoonPractice/No2480.java
@@ -1,7 +1,36 @@
 package baekjoonPractice;
 
+import java.util.Scanner;
+
 public class No2480 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int dice1 = sc.nextInt();
+        int dice2 = sc.nextInt();
+        int dice3 = sc.nextInt();
+
+        if (dice1==dice2){
+            if(dice2==dice3){
+                System.out.println(10000+((dice3)*1000));
+            } else {
+                System.out.println(1000+((dice2)*100));
+            }
+        } else if (dice1==dice3){
+            System.out.println(1000+((dice3)*100));
+        }  else if (dice2==dice3){
+            System.out.println(1000+((dice2)*100));
+        } //case 3 - 1
+        else if (dice1>dice2) {
+            if (dice1>dice3){
+                System.out.println(dice1*100);
+            } else {
+                System.out.println(dice3*100);
+            }
+        } //case 3 -2
+        else if (dice2>dice3){
+            System.out.println(dice2*100);
+        } else {
+            System.out.println(dice3*100);
+        }
     }
 }
