2f97bc55d568202f01c74ed9eec87a23ecd00df7
hohyun0406 <hohyun0406@gmail.com>
2024-04-05 13:28:37 +0900

    240405 a+b loop

---
+++ src/baekjoonPractice/No10950.java
@@ -0,0 +1,15 @@
+package baekjoonPractice;
+
+import java.util.Scanner;
+
+public class No10950 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int caseNum = sc.nextInt();
+
+        for(int i=0;i<caseNum;i++){
+            System.out.println(sc.nextInt() + sc.nextInt());
+        }
+    }
+}
