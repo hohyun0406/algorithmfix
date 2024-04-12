888aecd955c9af5c58f3a31435593ecb1c530e12
hohyun0406 <hohyun0406@gmail.com>
2024-04-12 18:39:24 +0900

    240412 No25304 receipt Sum

---
+++ src/baekjoonPractice/No25304.java
@@ -0,0 +1,22 @@
+package baekjoonPractice;
+
+import java.util.Scanner;
+
+public class No25304 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long price = sc.nextLong();
+        int productTypeNumber = sc.nextInt();
+        long priceSum = 0;
+
+        for(int i =1;i<=productTypeNumber;i++){
+            priceSum += sc.nextLong() * sc.nextLong();
+        }
+
+        if (price==priceSum){
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
