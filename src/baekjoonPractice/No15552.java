432764e79cba1aa1630a7a2ccdf00ef036c9c2dc
hohyun0406 <hohyun0406@gmail.com>
2024-04-14 22:11:17 +0900

    240414 No15552 BufferReader Split

--- src/baekjoonPractice/No15552.java
+++ src/baekjoonPractice/No15552.java
@@ -1,19 +1,31 @@
 package baekjoonPractice;
 
 
-import java.io.*;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 
 public class No15552 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
+
         int T = Integer.parseInt(bufferedReader.readLine());
-        for(int i=0;i<T;i++){
-            st = new StringTokenizer(bufferedReader.readLine());
-            bufferedWriter.write((Integer.parseInt(st.nextToken()+Integer.parseInt(st.nextToken())))+"\n");
+
+        for (int i = 0; i < T; i++) {
+            String[] input = bufferedReader.readLine().split(" ");
+            int A = Integer.parseInt(input[0]);
+            int B = Integer.parseInt(input[1]);
+
+            int sum = A + B;
+
+            bufferedWriter.write(sum + "\n");
         }
+
+        bufferedWriter.flush();
         bufferedWriter.close();
+        bufferedReader.close();
     }
 }
