731fc32b1e6b177bc9c811b66b4470d956efcab7
hohyun0406 <hohyun0406@gmail.com>
2024-05-23 23:47:28 +0900

    240523 minmax index

---
+++ src/baekjoonStep4/No2562.java
@@ -0,0 +1,23 @@
+package baekjoonStep4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No2562 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int max = 0;
+        int count = 0;
+        for (int i = 1; i < 10; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (max < num) {
+                max = num;
+                count = i;
+            }
+        }
+        sb.append(max).append("\n").append(count);
+        System.out.println(sb);
+    }
+}
